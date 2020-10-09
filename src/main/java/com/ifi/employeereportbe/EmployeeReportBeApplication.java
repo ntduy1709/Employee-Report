6d2450f9d3bdb7b525b5b559288954db2915c3c7
ntduy1709 <ntduy1709@users.noreply.github.com>
2020-10-09 09:56:50 +0700

    Init Project Back-End

---
+++ src/main/java/com/ifi/employeereportbe/EmployeeReportBeApplication.java
@@ -0,0 +1,13 @@
+package com.ifi.employeereportbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReportBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeReportBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifi/employeereportbe/EmployeeReportBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifi.employeereportbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReportBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
